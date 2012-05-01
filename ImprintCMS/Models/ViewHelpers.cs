aab825450a56f75589793d7723fd5746db4cc666
Peder Skou <peder.skou@gyldendal.no>
2012-05-01 20:34:31 +0200

    Separate resource file for site. Link to excerpt from book details

--- ImprintCMS/Models/ViewHelpers.cs
+++ ImprintCMS/Models/ViewHelpers.cs
@@ -29,11 +29,11 @@ namespace ImprintCMS.Models
 
 		public static string ToBookListDate(this DateTime source)
 		{
-			if (source <= DateTime.Today) return Phrases.LabelIsPublished;
-			var formatString = Phrases.LabelWillBePublished + " {0} " + source.ToString("MMMM");
-			if (source.Day < 11) return String.Format(formatString, Phrases.UnitMonthPremio);
-			if (source.Day < 21) return String.Format(formatString, Phrases.UnitMonthMedio);
-			return String.Format(formatString, Phrases.UnitMonthUltimo);
+			if (source <= DateTime.Today) return SitePhrases.LabelIsReleased;
+			var formatString = SitePhrases.LabelToBeReleased + " {0} " + source.ToString("MMMM");
+			if (source.Day < 11) return String.Format(formatString, SitePhrases.UnitMonthPremio);
+			if (source.Day < 21) return String.Format(formatString, SitePhrases.UnitMonthMedio);
+			return String.Format(formatString, SitePhrases.UnitMonthUltimo);
 		}
 
 		public static HtmlString Twitter(this HtmlHelper helper)
@@ -56,7 +56,7 @@ namespace ImprintCMS.Models
 		<input type=""text"" name=""q"" id=""query"" value=""{1}"" placeholder=""{2}"" />
 		<input type=""submit"" id=""button"" value=""{3}"" /></p>
 </fieldset>
-</form>", action, query, Phrases.LabelSearchPlaceholder, Phrases.LabelSearch));
+</form>", action, query, SitePhrases.LabelSearchPlaceholder, SitePhrases.LabelSearch));
 		}
 
 	}
