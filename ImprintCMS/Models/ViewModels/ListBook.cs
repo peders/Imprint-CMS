720f5ef101aab531ebe8b7f9432e375dc0096926
Peder Skou <peder.skou@gyldendal.no>
2013-07-09 13:34:57 +0200

    Sort books without rights holder last

--- ImprintCMS/Models/ViewModels/ListBook.cs
+++ ImprintCMS/Models/ViewModels/ListBook.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Collections.Generic;
+using System.Web;
 using System.Web.Mvc;
 using System.Linq;
 using System;
@@ -11,7 +12,9 @@ namespace ImprintCMS.Models.ViewModels
         public string Subtitle { get; set; }
         public Genre Genre { get; set; }
         public int ReleaseYear { get; set; }
-        public string RightsHolders { get; set; }
+        public string RightsHoldersDisplay { get; set; }
+        public bool HasRightsHolders { get; set; }
+        public IEnumerable<Person> RightsHolders { get; set; }
         public string Url { get; set; }
         public HtmlString PersonPageEntry { get; set; }
         public Edition CoverEdition { get; set; }
@@ -22,13 +25,15 @@ namespace ImprintCMS.Models.ViewModels
             Subtitle = book.Subtitle;
             Genre = book.Genre;
             Url = url.Action("details", "books", new { id = book.Id });
+            HasRightsHolders = book.Relations.Any();
+            RightsHolders = book.Relations.OrderBy(r => r.SequenceIdentifier).Select(r => r.Person);
             if (!book.Relations.Any())
             {
-                RightsHolders = "–";
+                RightsHoldersDisplay = "–";
             }
             else
             {
-                RightsHolders = String.Join(" / ", book.Relations.OrderBy(r => r.SequenceIdentifier).Select(r => r.ReversePersonName));
+                RightsHoldersDisplay = String.Join(" / ", book.Relations.OrderBy(r => r.SequenceIdentifier).Select(r => r.ReversePersonName));
             }
             if (!book.Editions.Any())
             {
