f2997dffc49153fed48b1c14f2740a6dccca5df7
Peder Skou <peder.skou@gyldendal.no>
2013-09-26 09:52:56 +0200

    SQL: AlternativeNotForSaleMessage on Edition
    Create column

--- ImprintCMS/Models/Edition.cs
+++ ImprintCMS/Models/Edition.cs
@@ -4,31 +4,33 @@ using System.Web.Mvc;
 
 namespace ImprintCMS.Models
 {
-	[MetadataType(typeof(EditionMetadata))]
-	public partial class Edition
-	{
-		public string Name
-		{
-			get
-			{
-				return String.Format("{0} ({1} – {2})", Book.Title, Number, Binding.Name);
-			}
-		}
-	}
+    [MetadataType(typeof(EditionMetadata))]
+    public partial class Edition
+    {
+        public string Name
+        {
+            get
+            {
+                return String.Format("{0} ({1} – {2})", Book.Title, Number, Binding.Name);
+            }
+        }
+    }
 
-	public class EditionMetadata
-	{
-		[Required(ErrorMessage = "*")]
-		public int BookId { get; set; }
-		[Required(ErrorMessage = "*")]
-		public int BindingId { get; set; }
-		[Required(ErrorMessage = "*")]
-		public DateTime ReleaseDate { get; set; }
-		[Required(ErrorMessage = "*")]
-		public int Number { get; set; }
-		[Required(ErrorMessage = "*")]
-		public string Isbn { get; set; }
+    public class EditionMetadata
+    {
+        [Required(ErrorMessageResourceType = typeof(Phrases), ErrorMessageResourceName = "ValidationRequiredDefault")]
+        public int BookId { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Phrases), ErrorMessageResourceName = "ValidationRequiredDefault")]
+        public int BindingId { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Phrases), ErrorMessageResourceName = "ValidationRequiredDefault")]
+        public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Phrases), ErrorMessageResourceName = "ValidationRequiredDefault")]
+        public int Number { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Phrases), ErrorMessageResourceName = "ValidationRequiredDefault")]
+        public string Isbn { get; set; }
         [AllowHtml]
         public string Blurb { get; set; }
-	}
+        [StringLength(50, ErrorMessageResourceType = typeof(Phrases), ErrorMessageResourceName = "ValidationStringLength50")]
+        public string AlternativeNotForSaleMessage { get; set; }
+    }
 }
